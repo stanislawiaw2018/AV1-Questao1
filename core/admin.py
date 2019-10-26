570a09c85f6d73d1ea0da9c60380039b10d0bfa1
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-10-26 14:08:53 -0300

    Criado a função de acompanhamento do vencimento das despesas

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,23 @@
+
 from django.contrib import admin
+from core.models import controle
+
 # default: "Administração do Django"
 admin.site.site_header = 'Painel de Controle'
 # default: "Administração do Site"
 admin.site.index_title = 'Recursos'
 # default: ”Site de administração do Django"
 admin.site.site_title = 'Título HTML do Site'
+
+class controleAdmin(admin.ModelAdmin):
+    list_display = (
+        'data_criacao','tipo_despesa','vencimento','quitacao','a_vencer'
+    )
+    list_filter = (
+        'vencimento','quitacao',)
+
+
+
+    class Meta:
+        ordering = ('vencimento', 'forma_pagamento')
+admin.site.register(controle,controleAdmin)
--- core/models.py
+++ core/models.py
@@ -1,26 +1,55 @@
-from datetime import timezone
-
+from django.utils import timezone
+from datetime import datetime
 from django.db import models
 
 class controle(models.Model):
     TIPO_GASTO =[
-        ("1","Remedio"),
-        ("2","Roupa"),
-        ("3","Alimentação"),
-        ("4","Educação"),
-        ("5","Transporte"),
-        ("6","Outros"),
+        ("Remedio","Remedio"),
+        ("Roupa","Roupa"),
+        ("Alimentação","Alimentação"),
+        ("Educação","Educação"),
+        ("Transporte","Transporte"),
+        ("Outros","Outros"),
     ]
     PAGAMENTO =[
         ('CAD', 'cartao de debito'),
-        ('CAC', 'cartao de credito')
-        ('CRE', 'cartao Crediario')
+        ('CAC', 'cartao de credito'),
+        ('CRE', 'cartao Crediario'),
         ('CHE', 'cheque'),
         ('DI', 'dinheiro'),
     ]
-    data_criacao = models.DateTimeField('Data_Criação',default=timezone.now)
-    tipo_despesa = models.CharField('Tipo_Despesa',max_length=1,choices=TIPO_GASTO,default='6')
+    data_criacao = models.DateTimeField('Data Criação',auto_now_add=True)
+    tipo_despesa = models.CharField('Tipo Despesa',max_length=10,choices=TIPO_GASTO,default='6')
     descricao = models.TextField('Descrição')
-    forma_pagamento = models.CharField('forma_pagamento',max_length=3,choices=PAGAMENTO,default='DI')
-    vencimento = models.DateField('Data_vencimento')
+    forma_pagamento = models.CharField('forma pagamento',max_length=3,choices=PAGAMENTO,default='DI')
+    vencimento = models.DateField('Data vencimento')
     quitacao = models.BooleanField('Quitação')
+
+    class Meta:
+        verbose_name_plural = 'Controles de gastos'
+        verbose_name = 'Controle de gasto'
+
+    def __str__(self):
+        return "{} - {}".format(
+            self.data_criacao.strftime('%d/%m/%Y'),
+            self.tipo_despesa
+        )
+    def expira(self):
+        data_atual = datetime.now().date()
+        data = self.vencimento - data_atual
+        if data.days < 0 and self.quitacao == False:
+            return 'Fatura vencida!'
+
+        elif data.days < 0 and self.quitacao == True:
+            return 'Fatura já foi paga!'
+
+        elif data.days == 0:
+            return 'Sua fatura se vence hoje!'
+
+        elif data.days <= 2:
+            return 'Fatura com menos de 2 dias para o vencimento, por favor efetuar o pagamento!'
+
+        else:
+            return '{} dias para o vencimento'.format(data.days)
+
+    a_vencer = property(expira)
